d049a05558ec5b61ad4a8cf7cdfb3a22c9be48ef
turgunov24 <learningplatforms.turgunov@gmail.com>
2022-06-19 16:46:41 +0500

    four edit

--- src/components/navbarTop/main/navbarTop.jsx
+++ src/components/navbarTop/main/navbarTop.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import "./style.css"
+import React from "react";
+import "./style.css";
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
 export const NavbarTop = () => {
-    return (
-        <>
-            <div className="navbarTop">
-                <div className="searchDiv">
-                    <div className="calendarDiv">
-                        <h2>Jaegar Resto</h2>
-                        <p>Tuesday, 2 Feb 2021</p>
-                    </div>
-                    <input type="text" placeholder='Search for food, coffe, etc..' />
-                </div>
-                <div className="tableDiv">
-                navbarTop
-                
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="navbarTop">
+        <div className="searchDiv">
+          <div className="calendarDiv">
+            <h2>Jaegar Resto</h2>
+            <p>Tuesday, 2 Feb 2021</p>
+          </div>
+          <label>
+            <span><SearchRoundedIcon/></span>
+            <input type="text" placeholder="Search for food, coffe, etc.." />
+          </label>
+        </div>
+        <div className="tableDiv">navbarTop</div>
+      </div>
+    </>
+  );
 };
