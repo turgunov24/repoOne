a742fd5c621542d75817ea56fd1ca9daac8bded7
turgunov24 <learningplatforms.turgunov@gmail.com>
2022-06-19 16:17:36 +0500

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { NavbarLeft } from './navbarLeft/main/navbarLeft';
+import { NavbarTop } from './navbarTop/main/navbarTop';
+
+export default function App() {
+  return (
+    <>
+      <NavbarLeft/>
+      <NavbarTop/>
+    </>
+  );
+}
